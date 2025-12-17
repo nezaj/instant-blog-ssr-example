f0307eb60fe168ab65a0e1d0daa5b5f04f248bcd
Joe Averbukh <joeaverbukh@gmail.com>
2025-12-16 16:42:41 -0800

    sm

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,8 +3,12 @@ import schema from "../instant.schema";
 
 export const db = init({
   appId: process.env.NEXT_PUBLIC_INSTANT_APP_ID!,
+
+  // For testing against a local instance of InstantDB
+  // For apps on InstantDB Cloud, remove these two lines
   apiURI: "http://localhost:8888",
   websocketURI: "ws://localhost:8888/runtime/session",
+
   cookieEndpoint: "/api/instant",
   schema,
   useDateObjects: true,
